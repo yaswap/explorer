049585e32f165c067543071cf077511b96e990dc
Chau Thai Nguyen <chaunguyen4392@gmail.com>
2021-04-11 10:11:57 +0700

    Update csv info when UTXO is used

--- models/csvinfo.js
+++ models/csvinfo.js
@@ -1,14 +1,13 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
  
-var CsvInfoSchema = new Schema({
-    a_id: { type: String, unique: true, index: true, default: ""},
-    txid: { type: String, lowercase: true, unique: true, index: true},
-    vout: { type: Number, default: 0 },
+var CsvUtxoInfoSchema = new Schema({
+    a_id: { type: String, index: true},
+    txid: { type: String, lowercase: true, index: true},
     isused: { type: Boolean, default: false },
     amount: { type: Number, default: 0 },
     timetouse_number: { type: Number, default: 0 },
     timetouse_string: { type: String, default: "" },
   });
 
-module.exports = mongoose.model('CsvInfo', TimeLockSchema);
+module.exports = mongoose.model('CsvInfo', CsvUtxoInfoSchema);
