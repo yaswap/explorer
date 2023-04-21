f5d97d8bd254ad1dd9e622dc8563806a5f487bb1
Nguyen Thai Chau <chaunguyen4392@gmail.com>
2023-04-21 12:21:37 +0700

    Run prettier on all files

--- models/timelock.js
+++ models/timelock.js
@@ -1,15 +1,15 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
-  
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
 var TimeLockSchema = new Schema({
-    a_id: { type: String, unique: true, index: true, default: ""},
-    redeemscript: { type: String, unique: true, index: true, default: ""},
-    iscltv: { type: Boolean, default: true },
-    istimebased: { type: Boolean, default: true },
-    locktime: { type: Number, default: 0 },
-    description: { type: String, default: "" },
-    csvinfo: { type: Array, default: [] },
-    balance: {type: Number, default: 0, index: true},
+  a_id: { type: String, unique: true, index: true, default: '' },
+  redeemscript: { type: String, unique: true, index: true, default: '' },
+  iscltv: { type: Boolean, default: true },
+  istimebased: { type: Boolean, default: true },
+  locktime: { type: Number, default: 0 },
+  description: { type: String, default: '' },
+  csvinfo: { type: Array, default: [] },
+  balance: { type: Number, default: 0, index: true },
 });
 
 module.exports = mongoose.model('TimeLock', TimeLockSchema);
--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,13 +1,12 @@
-var mongoose = require('mongoose')
-  , db = require('../lib/database')
-  , Tx = require('../models/tx')  
-  , Address = require('../models/address')  
-  , settings = require('../lib/settings');
-
+var mongoose = require('mongoose'),
+  db = require('../lib/database'),
+  Tx = require('../models/tx'),
+  Address = require('../models/address'),
+  settings = require('../lib/settings');
 
 var COUNT = 5000; //number of blocks to index
 
-function exit() {  
+function exit() {
   mongoose.disconnect();
   process.exit(0);
 }
@@ -16,25 +15,25 @@ var dbString = 'mongodb://' + settings.dbsettings.user;
 dbString = dbString + ':' + settings.dbsettings.password;
 dbString = dbString + '@' + settings.dbsettings.address;
 dbString = dbString + ':' + settings.dbsettings.port;
-dbString = dbString + "/IQUIDUS-BENCHMARK";
+dbString = dbString + '/IQUIDUS-BENCHMARK';
 
-mongoose.connect(dbString, function(err) {
+mongoose.connect(dbString, function (err) {
   if (err) {
     console.log('Unable to connect to database: %s', dbString);
     console.log('Aborting');
     exit();
   }
-  Tx.remove({}, function(err) { 
-    Address.remove({}, function(err2) { 
+  Tx.remove({}, function (err) {
+    Address.remove({}, function (err2) {
       var s_timer = new Date().getTime();
-      db.update_tx_db(settings.coin, 1, COUNT, settings.update_timeout, function(){
+      db.update_tx_db(settings.coin, 1, COUNT, settings.update_timeout, function () {
         var e_timer = new Date().getTime();
-        Tx.count({}, function(txerr, txcount){
-          Address.count({}, function(aerr, acount){
+        Tx.count({}, function (txerr, txcount) {
+          Address.count({}, function (aerr, acount) {
             var stats = {
               tx_count: txcount,
               address_count: acount,
-              seconds: (e_timer - s_timer)/1000,
+              seconds: (e_timer - s_timer) / 1000,
             };
             console.log(stats);
             exit();
