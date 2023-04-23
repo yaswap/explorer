fa137017d6f2bd62ac3b24978c6cbd415626ecdf
Nguyen Thai Chau <chaunguyen4392@gmail.com>
2023-04-23 09:26:20 +0700

    Redesign database model AddressUtxo, TimeLock, AddressUtxoMempool

--- models/addressutxo.js
+++ models/addressutxo.js
@@ -1,12 +1,21 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
+var TimelockUtxoInfoSchema = new Schema({
+  iscltv: Boolean,
+  istimebased: Boolean,
+  locktime: Number,
+  timetouse_number: Number,
+  timetouse_string: String,
+});
+
 var AddressUtxoSchema = new Schema({
   a_id: { type: String, index: true },
   txid: { type: String, lowercase: true, index: true },
   vout: { type: Number, default: 0 },
   isused: { type: Boolean, default: false },
   amount: { type: Number, default: 0 },
+  timelockinfo: TimelockUtxoInfoSchema, // by default (not timelock UTXO) = undefined
 });
 
 AddressUtxoSchema.index({ a_id: 1, isused: 1 });
--- models/addressutxomempool.js
+++ models/addressutxomempool.js
@@ -1,6 +1,14 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
+var TimelockUtxoInfoSchema = new Schema({
+  iscltv: Boolean,
+  istimebased: Boolean,
+  locktime: Number,
+  timetouse_number: Number,
+  timetouse_string: String,
+});
+
 // Store all UTXOs of addresses which relate to transactions in mempool
 var AddressUtxoMempoolSchema = new Schema({
   blockheight: { type: Number, default: 0, index: true },
@@ -10,6 +18,7 @@ var AddressUtxoMempoolSchema = new Schema({
   vout: { type: Number, default: 0 },
   isused: { type: Boolean, default: false },
   amount: { type: Number, default: 0 },
+  timelockinfo: TimelockUtxoInfoSchema, // by default (not timelock UTXO) = undefined
 });
 
 AddressUtxoMempoolSchema.index({ blockheight: 1, a_id: 1, isused: 1 });
